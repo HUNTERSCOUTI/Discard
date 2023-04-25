7f93c4285930d9aca24630973fc30af0bfa9c826
Zilas Jørgensen <Zilas2004@gmail.com>
2023-04-25 15:02:24 +0200

    Server Addition

---
+++ Server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Server
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Server server = new();
+
+            server.Start();
+        }
+    }
+}
