a7da518b7e4bb4bcf4180a69bf58e601da8c848d
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-04-26 13:50:55 +0200

    FrienList

---
+++ Discard/MVVM/Models/FriendModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscardSERVER.Class_Models
+{
+    public class FriendModel
+    {
+        public int FriendID { get; set; }
+        public string[]? Messages { get; set; }
+    }
+}
--- Discard/MVVM/ViewModels/FriendsListVM.cs
+++ Discard/MVVM/ViewModels/FriendsListVM.cs
@@ -1,6 +1,48 @@
-﻿namespace Client.MVVM.ViewModels;
+﻿using System.Collections.ObjectModel;
+using Client.MVVM.Utilities;
+using DiscardSERVER.Class_Models;
 
-public class FriendsListVM
+namespace Client.MVVM.ViewModels;
+
+public class FriendsListVM : ViewModelBase
 {
-    
+    #region Properties
+
+    private object _currentView = new HomeVM();
+
+    public object CurrentView
+    {
+        get => _currentView;
+        set
+        {
+            _currentView = value;
+            OnPropertyChanged();
+        }
+    }
+
+    private ObservableCollection<FriendModel> _friends { get; set; }
+
+    public ObservableCollection<FriendModel> FriendList
+    {
+        get => _friends;
+        set
+        {
+            _currentView = value;
+            OnPropertyChanged();
+        }
+    }
+
+    #endregion
+
+
+    public FriendsListVM()
+    {
+        _friends = new ObservableCollection<FriendModel>();
+        _friends.Add(new FriendModel());
+        _friends.Add(new FriendModel());
+        _friends.Add(new FriendModel());
+        _friends.Add(new FriendModel());
+        _friends.Add(new FriendModel());
+        
+    }
 }
