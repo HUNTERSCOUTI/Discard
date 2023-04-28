de153e84ed7b0cb60fb34c0e1a508347ee661f51
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-04-28 19:49:19 +0200

    !Errors keeps occuring!

--- Discard/MainWindow.xaml.cs
+++ Discard/MainWindow.xaml.cs
@@ -19,17 +19,17 @@ public partial class MainWindow : Window
 {
     public MainWindow()
     {
-        try
-        {
-            ClientConnection client = new();
-
-            Thread thread = new(client.Run);
-            thread.Start();
-        }
-        catch (Exception e)
-        {
-            System.Windows.MessageBox.Show("Could Not Connect to the Server");
-        }
+        // try
+        // {
+        //     ClientConnection client = new();
+        //
+        //     Thread thread = new(client.Run);
+        //     thread.Start();
+        // }
+        // catch (Exception e)
+        // {
+        //     System.Windows.MessageBox.Show("Could Not Connect to the Server");
+        // }
 
         //Fix, does not start WPF program on another thread
         InitializeComponent();
