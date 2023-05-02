0c0bc3d85cd1711a137483d583ef7632cf6afa50
Kevin Bamwesa Ishara <kevi3758@gmail.com>
2023-05-02 16:45:34 +0200

    * Server runs on Loopback, * Received message is added to the list of messages

--- Discard/Networking/ClientConnection.cs
+++ Discard/Networking/ClientConnection.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Windows;
+using Client.MVVM.ViewModels;
 
 namespace Client.Networking
 {
@@ -13,27 +14,18 @@ namespace Client.Networking
 
         public void Run()
         {
-            //Connect to own PC
-            ConnectToServer(IPAddress.Loopback, PORT);
             //Connect to Zilas
+            ConnectToServer("127.0.0.1", PORT);
             //ConnectToServer(IPAddress.Parse("172.22.240.1"), PORT);
-            while (true)
-            {
-                string send = Console.ReadLine();
-                SendMessage(send);
-            }
         }
 
-        public void ConnectToServer(IPAddress ip, int port)
+        public void ConnectToServer(string ip, int port)
         {
-            
-                Connection = new TcpClient();
-                Connection.Connect(ip, port);
+            Connection = new TcpClient();
+            Connection.Connect(ip, port);
 
-                Thread thread = new Thread(Listener);
-                thread.Start();
-                Console.WriteLine("Connected To Server");
-            
+            Thread thread = new Thread(Listener);
+            thread.Start();
         }
 
         /// <summary>
@@ -44,13 +36,20 @@ namespace Client.Networking
             byte[] buffer = new byte[4096];
             NetworkStream stream = Connection.GetStream();
 
-            while (Connection.Connected)
+            try
             {
-                int read = stream.Read(buffer, 0, buffer.Length);
-                string messageFromServer = Encoding.UTF8.GetString(buffer, 0, read);
-                //MAKE displayable on WPF HERE
+                while (Connection.Connected)
+                {
+                    int read = stream.Read(buffer, 0, buffer.Length);
+                    string messageFromServer = Encoding.UTF8.GetString(buffer, 0, read);
+                    //MAKE displayable on WPF HERE
 
-                buffer = new byte[4096];
+                    GlobalChatVM.MessageHistory.Add(messageFromServer);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Connection to the server has been lost", "Error");
             }
         }
 
@@ -58,7 +57,7 @@ namespace Client.Networking
         {
             if (Connection.Connected)
             {
-                byte[] bytes = Encoding.UTF8.GetBytes("Zilas: " + message);
+                byte[] bytes = Encoding.UTF8.GetBytes(message);
                 NetworkStream stream = Connection.GetStream();
                 stream.Write(bytes, 0, bytes.Length);
             }
