91be0d246d82797c03c9c6f3675b47213829a7c1
Kevin(Home) <kevi3758@gmail.com>
2023-06-13 01:51:47 +0200

    Moved Send Message Method into it own static class

---
+++ Discard/Networking/Models/MessageSender.cs
@@ -0,0 +1,25 @@
+﻿using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+
+namespace Client.Networking.Models;
+
+public static class MessageSender
+{
+    public static void SendMessage(string message, TcpClient Connection)
+    {
+        try
+        {
+            if (Connection.Connected)
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(message);
+                NetworkStream stream = Connection.GetStream();
+                stream.Write(bytes, 0, bytes.Length);
+            }
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show($"Could not send message to the server \n{e.Message}", "Error");
+        }
+    }
+}
